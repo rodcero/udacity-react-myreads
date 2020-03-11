c93bde9df2a186e710da6325b9047cd498e00f48
Ernie Rodriguez <ernie@Ernies-MacBook-Pro-3.local>
2020-03-11 14:23:51 -0700

    book component now makes use of book passed in as a prop

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Book() {
+export default function Book({ book }) {
   return (
     <div className="book">
       <div className="book-top">
@@ -9,8 +9,7 @@ export default function Book() {
           style={{
             width: 128,
             height: 193,
-            backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+            backgroundImage: `url("${book.imageLinks.thumbnail}")`,
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -25,8 +24,10 @@ export default function Book() {
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">
+        {book.authors ? book.authors.join(',') : ''}
+      </div>
     </div>
   );
 }
